094efbf4c782dc71cc4cacafafdda6b2794a924a
Gianfranco Palumbo <gianpa@gmail.com>
2018-09-17 17:31:48 +0300

    Update ContributePrompt adding a className to FieldGroup

--- src/components/ContributePrompt.js
+++ src/components/ContributePrompt.js
@@ -4,12 +4,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Modal, Form, Button } from 'react-bootstrap'
+import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap'
 import { FieldGroup } from './'
 
 export default class ContributePrompt extends Component {
   constructor(props) {
     super(props)
-    this.state = { show: false, summary: '', details: '', supportingInfo: '', type: 'select' }
+    this.state = { show: false, summary: '', details: '', type: 'select' }
     this.canSubmit = this.canSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
     this.okHandler = this.okHandler.bind(this)
@@ -23,7 +24,7 @@ export default class ContributePrompt extends Component {
   static defaultProps = {}
 
   open() {
-    this.setState({ show: true, summary: '', details: '', supportingInfo: '', type: 'select' })
+    this.setState({ show: true, summary: '', details: '', type: 'select' })
   }
 
   close() {
@@ -44,13 +45,13 @@ export default class ContributePrompt extends Component {
   }
 
   canSubmit() {
-    const { details, supportingInfo, summary, type } = this.state
+    const { details, summary, type } = this.state
 
-    return summary.length > 10 && type !== 'select' && details.length > 10 && supportingInfo.length > 10
+    return summary.length > 10 && type !== 'select' && details.length > 10
   }
 
   render() {
-    const { details, supportingInfo, summary, show, type, resolution } = this.state
+    const { details, summary, show, type, resolution } = this.state
     return (
       <Modal show={show} onHide={this.close}>
         <Form>
@@ -58,36 +59,37 @@ export default class ContributePrompt extends Component {
             <Modal.Title>Describe the changes in this curation</Modal.Title>
           </Modal.Header>
           <Modal.Body>
+            <div>
+              <FieldGroup
+                className="inlineBlock pull-right"
+                name="type"
+                label="Type"
+                value={type || 'select'}
+                onChange={this.handleChange}
+                placeholder="select"
+                componentClass="select"
+                required
+              >
+                <option value="select" disabled>
+                  select
+                </option>
+                <option value="missing">Missing</option>
+                <option value="incorrect">Incorrect</option>
+                <option value="incomplete">Incomplete</option>
+                <option value="ambiguous">Ambiguous</option>
+                <option value="other">Other</option>
+              </FieldGroup>
+            </div>
             <FieldGroup
               name="summary"
               type="text"
               label="Summary"
               value={summary || ''}
               onChange={this.handleChange}
-              placeholder="Short summary of changes. Like a commit message..."
+              placeholder="Short summary of changes. Like a commit message. Maximum 100 characters"
               maxLength={100}
-              componentClass="textarea"
-              rows="2"
               required
             />
-            <FieldGroup
-              name="type"
-              label="Type"
-              value={type || 'select'}
-              onChange={this.handleChange}
-              placeholder="select"
-              componentClass="select"
-              required
-            >
-              <option value="select" disabled>
-                select
-              </option>
-              <option value="missing">Missing</option>
-              <option value="incorrect">Incorrect</option>
-              <option value="incomplete">Incomplete</option>
-              <option value="ambiguous">Ambiguous</option>
-              <option value="other">Other</option>
-            </FieldGroup>
             <FieldGroup
               name="details"
               type="text"
@@ -101,27 +103,16 @@ export default class ContributePrompt extends Component {
               required
             />
             <FieldGroup
-              name="supportingInfo"
-              type="text"
-              label="Supporting Info"
-              value={supportingInfo || ''}
-              onChange={this.handleChange}
-              placeholder="References to docs where the new data was found, pointer to public conversations with the affected project team"
-              maxLength={300}
-              componentClass="textarea"
-              rows="3"
-              required
-            />
-            <FieldGroup
               name="resolution"
               type="text"
               label="Resolution"
               value={resolution || ''}
               onChange={this.handleChange}
-              placeholder="What does this PR do to address the issue"
+              placeholder="What does this PR do to address the issue? Include references to docs where the new data was found and, for example, links to public conversations with the affected project team"
               maxLength={300}
               componentClass="textarea"
               rows="3"
+              required
             />
           </Modal.Body>
           <Modal.Footer>
