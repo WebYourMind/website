e7a890d751ffd9bb6c3231f31724c41d56573a1d
Simone Torrisi <storrisi@gmail.com>
2018-10-04 17:41:38 +0200

    Removed test

--- src/components/__tests__/FullDetailPage.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import FullDetailPage from '../FullDetailView/FullDetailPage'
-
-describe('FullDetailPage', () => {
-  it('renders without crashing', () => {
-    shallow(<FullDetailPage modalView={false} />)
-  })
-})
