a7eea174e5535a6593087314b1a83f90c0e431f7
Riccardo Genova <riccardogenova@icloud.com>
2018-10-16 00:01:15 +0200

    [feat] Add switch case for support all providers

--- src/components/PageDefinitions.js
+++ src/components/PageDefinitions.js
@@ -277,7 +277,6 @@ class PageDefinitions extends AbstractPageDefinitions {
 
   onTextDrop = url => {
     const path = EntitySpec.fromUrl(url)
-    console.log(path)
     this.onAddComponent(path)
   }
 
