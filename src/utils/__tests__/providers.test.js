1add260126b579e729e828c3a75eaf83a8b3e722
Simone Torrisi <storrisi@gmail.com>
2018-11-06 19:32:55 +0100

    Providers classes with tests

--- src/utils/__tests__/providers.test.js
+++ src/utils/__tests__/providers.test.js
@@ -1,8 +1,69 @@
-import { Provider, NpmProvider } from '../providers'
+import { Provider } from '../providers'
 
 describe('Provider', () => {
-  it('returns a provider object', () => {
+  it('returns a npm path without version', async () => {
     const provider = new Provider('https://www.npmjs.com/package/deep-diff')
-    provider.get()
+    const result = await provider.get()
+    expect(result).toEqual({ errors: 'npmjs.com need a version to be imported' })
+  })
+  it('returns a npm path with a version', async () => {
+    const provider = new Provider('https://www.npmjs.com/package/async/v/2.6.0')
+    const result = await provider.get()
+    expect(result).toEqual('npm/npmjs/-/async/2.6.0')
+  })
+  it('returns a github path with a version', async () => {
+    const provider = new Provider('https://github.com/github/fetch/releases/tag/v3.0.0')
+    const result = await provider.get()
+    expect(result).toEqual('git/github/github/fetch/v3.0.0')
+  })
+  it('returns a github path with a version', async () => {
+    const provider = new Provider('https://github.com/github/fetch/commit/d4ed806fdcbdeaef707d27f6c88943f0336a647d')
+    const result = await provider.get()
+    expect(result).toEqual('git/github/github/fetch/d4ed806fdcbdeaef707d27f6c88943f0336a647d')
+  })
+  it('returns a github path without version', async () => {
+    const provider = new Provider('https://github.com/github/fetch')
+    const result = await provider.get()
+    expect(result).toEqual({ errors: 'github.com need a version to be imported' })
+  })
+  it('returns a maven path with a version', async () => {
+    const provider = new Provider('https://mvnrepository.com/artifact/cc.mallet/mallet/2.0.8')
+    const result = await provider.get()
+    expect(result).toEqual('maven/mavencentral/cc.mallet/mallet/2.0.8')
+  })
+  it('returns a maven path without a version', async () => {
+    const provider = new Provider('https://mvnrepository.com/artifact/cc.mallet/mallet')
+    const result = await provider.get()
+    expect(result).toEqual({ errors: 'mvnrepository.com need a version to be imported' })
+  })
+  it('returns a pypi path with a version', async () => {
+    const provider = new Provider('https://pypi.org/project/pyaml/17.8.0/')
+    const result = await provider.get()
+    expect(result).toEqual('pypi/pypi/-/pyaml/17.8.0')
+  })
+  it('returns a pypi path without a version', async () => {
+    const provider = new Provider('https://pypi.org/project/pyaml/')
+    const result = await provider.get()
+    expect(result).toEqual({ errors: 'pypi.org need a version to be imported' })
+  })
+  it('returns a nuget path with a version', async () => {
+    const provider = new Provider('https://nuget.org/packages/NuGet.VisualStudio/2.8.2')
+    const result = await provider.get()
+    expect(result).toEqual('nuget/nuget/-/NuGet.VisualStudio/2.8.2')
+  })
+  it('returns a nuget path without a version', async () => {
+    const provider = new Provider('https://www.nuget.org/packages/NuGet.VisualStudio/')
+    const result = await provider.get()
+    expect(result).toEqual({ errors: 'nuget.org need a version to be imported' })
+  })
+  it('returns a rubygem path with a version', async () => {
+    const provider = new Provider('https://rubygems.org/gems/tzinfo/versions/1.2.5')
+    const result = await provider.get()
+    expect(result).toEqual('gem/rubygems/-/tzinfo/1.2.5')
+  })
+  it('returns a rubygem path without a version', async () => {
+    const provider = new Provider('https://rubygems.org/gems/tzinfo')
+    const result = await provider.get()
+    expect(result).toEqual({ errors: 'rubygems.org need a version to be imported' })
   })
 })
--- src/utils/providers.js
+++ src/utils/providers.js
@@ -1,61 +1,133 @@
-const { setIfValue } = require('./utils')
-
 export class Provider {
   constructor(url) {
     const urlObject = new URL(url)
     this.pathname = urlObject.pathname.startsWith('/') ? urlObject.pathname.slice(1) : urlObject.pathname
     this.hostname = urlObject.hostname.replace('www.', '')
     this.urlPath = this.pathname.split('/')
-    this.providers = [new NpmProvider(this.urlPath, this.hostname), new GitHubProvider(this.urlPath, this.hostname)]
+    this.providers = [
+      new NpmProvider(this.urlPath, this.hostname),
+      new GitHubProvider(this.urlPath, this.hostname),
+      new MavenProvider(this.urlPath, this.hostname),
+      new PyPiProvider(this.urlPath, this.hostname),
+      new NugetProvider(this.urlPath, this.hostname),
+      new RubyGemProvider(this.urlPath, this.hostname)
+    ]
   }
 
-  get() {
-    Promise.all(this.providers)
+  async get() {
+    const path = await this.providers.reduce(async (result, provider) => {
+      const isValid = await this.isValid(this.hostname, provider.hostnames)
+      if (!isValid) return result
+      const returnedPath = await provider.getPath()
+      return returnedPath
+    }, false)
+    return path
   }
 
-  static checkValidHostname(providerHostnames, hostname) {
+  isValid(hostname, hostnames) {
+    return this.checkValidHostname(hostnames, hostname)
+  }
+
+  checkValidHostname(providerHostnames, hostname) {
     return providerHostnames.indexOf(hostname) >= 0 && true
   }
+
+  static providerErrorsFallback(provider) {
+    return { errors: `${provider} need a version to be imported` }
+  }
 }
 
-export class NpmProvider {
+export class GenericProvider {
   constructor(urlPath, hostname) {
     this.hostname = hostname
+    this.urlPath = urlPath
+  }
+}
+
+export class NpmProvider extends GenericProvider {
+  constructor(urlPath, hostname) {
+    super(urlPath, hostname)
     this.hostnames = ['npmjs.org', 'npmjs.com']
     this.path = 'npm/npmjs'
-    this.urlPath = urlPath
-    this.getUrl = this.fromUrl()
   }
 
-  fromUrl() {
-    console.log(Provider.checkValidHostname(this.hostnames, this.hostname))
-    console.log(this)
+  getPath() {
+    let nameSpace, name, revision
     if (this.urlPath.length === 5) {
-      ;[, this.nameSpace, this.name, , this.revision] = this.urlPath
+      ;[, nameSpace, name, , revision] = this.urlPath
     } else {
-      this.nameSpace = '-'
-      ;[, this.name, , this.revision] = this.urlPath
+      nameSpace = '-'
+      ;[, name, , revision] = this.urlPath
     }
+    return revision ? `${this.path}/${nameSpace}/${name}/${revision}` : Provider.providerErrorsFallback(this.hostname)
   }
 }
 
-export class GitHubProvider {
+export class GitHubProvider extends GenericProvider {
   constructor(urlPath, hostname) {
-    this.hostname = hostname
+    super(urlPath, hostname)
     this.hostnames = ['github.com']
     this.path = 'git/github'
-    this.urlPath = urlPath
-    this.getUrl = this.fromUrl()
   }
 
-  fromUrl() {
-    console.log(Provider.checkValidHostname(this.hostnames, this.hostname))
-    console.log(this)
+  getPath() {
+    let packageName, name, revision
     if (this.urlPath.length === 5) {
-      ;[, this.nameSpace, this.name, , this.revision] = this.urlPath
+      ;[packageName, name, , , revision] = this.urlPath
     } else {
-      this.nameSpace = '-'
-      ;[, this.name, , this.revision] = this.urlPath
+      ;[packageName, name, , revision] = this.urlPath
     }
+    return revision ? `${this.path}/${packageName}/${name}/${revision}` : Provider.providerErrorsFallback(this.hostname)
+  }
+}
+
+export class MavenProvider extends GenericProvider {
+  constructor(urlPath, hostname) {
+    super(urlPath, hostname)
+    this.hostnames = ['mvnrepository.com']
+    this.path = 'maven/mavencentral'
+  }
+
+  getPath() {
+    const [, name, version, revision] = this.urlPath
+    return revision ? `${this.path}/${name}/${version}/${revision}` : Provider.providerErrorsFallback(this.hostname)
+  }
+}
+
+export class PyPiProvider extends GenericProvider {
+  constructor(urlPath, hostname) {
+    super(urlPath, hostname)
+    this.hostnames = ['pypi.org']
+    this.path = 'pypi/pypi/-'
+  }
+
+  getPath() {
+    const [, name, revision] = this.urlPath
+    return revision ? `${this.path}/${name}/${revision}` : Provider.providerErrorsFallback(this.hostname)
+  }
+}
+
+export class NugetProvider extends GenericProvider {
+  constructor(urlPath, hostname) {
+    super(urlPath, hostname)
+    this.hostnames = ['nuget.org']
+    this.path = 'nuget/nuget/-'
+  }
+
+  getPath() {
+    const [, name, revision] = this.urlPath
+    return revision ? `${this.path}/${name}/${revision}` : Provider.providerErrorsFallback(this.hostname)
+  }
+}
+export class RubyGemProvider extends GenericProvider {
+  constructor(urlPath, hostname) {
+    super(urlPath, hostname)
+    this.hostnames = ['rubygems.org']
+    this.path = 'gem/rubygems/-'
+  }
+
+  getPath() {
+    const [, name, , revision] = this.urlPath
+    return revision ? `${this.path}/${name}/${revision}` : Provider.providerErrorsFallback(this.hostname)
   }
 }
