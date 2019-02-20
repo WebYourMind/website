977a9f7efd7ea196d0ea6c34191fd24f18bae135
Gianfranco Palumbo <gianpa@gmail.com>
2019-02-20 14:59:23 +0200

    display all buttons in a Button group

--- src/components/Navigation/Ui/ButtonWithTooltip.js
+++ src/components/Navigation/Ui/ButtonWithTooltip.js
@@ -6,7 +6,7 @@ import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 const ButtonWithTooltip = ({ children, tip }) => {
   return (
     <OverlayTrigger rootClose placement="top" overlay={<Tooltip id="tooltip">{tip}</Tooltip>}>
-      <div className="tooltipWrapper">{children}</div>
+      {children}
     </OverlayTrigger>
   )
 }
