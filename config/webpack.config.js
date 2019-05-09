c1fefea4823a1e603d9e915af7f1d4242642adda
Simone Torrisi <storrisi@gmail.com>
2019-05-09 19:39:59 +0200

    Fix webpack configuration

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -319,7 +319,6 @@ module.exports = function(webpackEnv) {
                 plugins: [
                   [
                     require.resolve('babel-plugin-named-asset-import'),
-                    require.resolve('@babel/plugin-proposal-class-properties'),
                     {
                       loaderMap: {
                         svg: {
